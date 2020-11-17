c28fdb876bd9758c032e460fd34180ea71fa9cbe
heehee812 <e.enyuu812@gmail.com>
2020-11-17 10:17:39 +0800

    Record R

--- mapping.cpp
+++ mapping.cpp
@@ -2,7 +2,31 @@
 using namespace std;
 
 /*global variable*/
-int row, col, Battery;
+int row, col, Battery, dir= 1;
+queue<int> q;
+stack<int> s;
+pair<int, int> R;
+
+/*class*/
+class Floor{
+    private:
+        int **floor;
+    public:
+        Floor(int **array): floor(array){}
+        void print_floor(){
+            for(int j= 0; j< row; j++){
+                for(int i= 0; i< col; i++)
+                    cout<<floor[j][i];
+                cout<<endl;
+            }
+        }         
+};
+
+class Point{
+    public:
+    int value;
+    pair<int, int> pos;
+};
 
 /*main function*/
 int main(int argc, char *argv[]){
@@ -11,7 +35,22 @@ int main(int argc, char *argv[]){
     string filename= argv[1];
     fstream ifile(filename, ios::in);
 
-    //get the floor size
     ifile>> row>> col>> Battery;
-    cout<< row<< ", "<< col<< ", "<< Battery<< endl;
+
+    //initialize a floor
+    int **floor= new int*[row];
+    for(int i= 0; i<row; i++)
+        floor[i]= new int[col];
+    
+    for(int j= 0; j<row; j++){
+        for(int i= 0; i<col&& !ifile.eof(); i++){
+            char c;
+            ifile>> c;
+            floor[j][i]= c-'0';
+            if(c== 'R')
+                R= make_pair(j, i);
+        }
+    }
+    Floor fr(floor);
+
 ;}
