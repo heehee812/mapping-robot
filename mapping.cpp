1fc1aa26e99c82567e9520fb803cbb43a5edabc4
heehee812 <e.enyuu812@gmail.com>
2020-11-17 12:41:48 +0800

    Add condition for dir=1 in aroud_point

--- mapping.cpp
+++ mapping.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 
 /*global variable*/
-typedef pair<int, int> pos;
+typedef pair<int, int> Pos;
+typedef queue<Pos> Queue;
+typedef stack<Pos> Stack;
 int row, col, Battery, dir= 1;
-queue<int> q;
-stack<int> s;
-pos R;
+Pos R;
 
 /*class*/
 class Floor{
     private:
         int **floor;
+        Queue readyQueue;
+        Stack waitingStack;
     public:
         Floor(int **array): floor(array){}
         void print_floor(){
@@ -20,7 +22,66 @@ class Floor{
                     cout<<floor[j][i];
                 cout<<endl;
             }
-        }         
+        }
+        void around_point(Pos ptr){
+            //decide what direction should go
+            if(ptr.first!= row-1){
+                cout<<"ptr.first!= bottom"<<endl;
+                if(floor[ptr.first-1][ptr.second]== 0){
+                    readyQueue.push(make_pair(ptr.first-1, ptr.second));
+                    if(dir){
+                        if(floor[ptr.first-1][ptr.second-1]== 0)
+                            readyQueue.push(make_pair(ptr.first-1, ptr.second-1));
+                    }
+                    else{
+                        if(floor[ptr.first-1][ptr.second+1]== 0)
+                            readyQueue.push(make_pair(ptr.first-1, ptr.second+1));
+                    }
+                }
+            }
+            if(dir){
+                cout<<"dir=1"<<endl;
+                if(floor[ptr.first][ptr.second-1]== 0){
+                    cout<<ptr.first<<","<<ptr.second-1<<endl;
+                    readyQueue.push(make_pair(ptr.first, ptr.second-1));
+                    if(floor[ptr.first-1][ptr.second-1]== 0){
+                        cout<<ptr.first-1<<","<<ptr.second-1<<endl;
+                        readyQueue.push(make_pair(ptr.first-1, ptr.second-1));
+                    }
+                }
+                if(floor[ptr.first-1][ptr.second]== 0){
+                    cout<<ptr.first-1<<","<<ptr.second<<endl;
+                    readyQueue.push(make_pair(ptr.first-1, ptr.second));
+                    if(floor[ptr.first-1][ptr.second+1]== 0){
+                        cout<<ptr.first-1<<","<<ptr.second+1<<endl;
+                        readyQueue.push(make_pair(ptr.first-1, ptr.second+1));
+                    }
+                }
+                if(floor[ptr.first][ptr.second+1]== 0){
+                    cout<<ptr.first<<","<<ptr.second+1<<endl;
+                    readyQueue.push(make_pair(ptr.first, ptr.second+1));
+                    if(ptr.first!= 0)
+                        if(floor[ptr.first+1][ptr.second+1]== 0){
+                            cout<<ptr.first+1<<","<<ptr.second+1<<endl;
+                            readyQueue.push(make_pair(ptr.first+1, ptr.second+1));
+                        }
+                }
+            }
+            else{
+                if(floor[ptr.first][ptr.second+1]== 0){
+                    readyQueue.push(make_pair(ptr.first, ptr.second+1));
+                    if(floor[ptr.first-1][ptr.second]== 0)
+                        readyQueue.push(make_pair(ptr.first-1, ptr.second+1));
+                }
+            }
+        }
+        void print_readyQueue(){
+            Queue copy= readyQueue;
+            while(!copy.empty()){
+                cout<<"("<<copy.front().first<<", "<<copy.front().second<<") ";
+                copy.pop();
+            }
+        }
 };
 
 /*main function*/
@@ -47,5 +108,6 @@ int main(int argc, char *argv[]){
         }
     }
     Floor fr(floor);
-
+    fr.around_point(R);
+    // fr.print_readyQueue();
 ;}
